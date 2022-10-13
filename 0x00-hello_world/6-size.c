11581d1e7e8d3e384ee3011bc657412751194f19
Fortunatus <wulnyefortunatus@gmail.com>
2022-10-13 13:43:54 -0700

    Hello world

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -9,14 +9,14 @@ int main(void)
 {
 int int_type;
 long int double_type;
-long long int long_double_type
+long long int long_double_type;
 char char_type;
 float float_type;
 
-printf("Size of a char: %lu byte(s)\n",sizeof(char_type));
-printf("Size of an int: %lu byte(s)\n",sizeof(int_type));
-printf("Size of a long int: %lu byte(s)\n",sizeof(double_type));
-printf("Size of a long long int: %lu byte(s)\n",sizeof(long_double_type));
-printf("Size of a float: %lu byte(s)\n",sizeof(floating_point));
+printf("Size of a char: %lu byte(s)\n",(unsigned long)sizeof(char_type));
+printf("Size of an int: %lu byte(s)\n",(unsigned long)sizeof(int_type));
+printf("Size of a long int: %lu byte(s)\n",(unsigned long)sizeof(double_type));
+printf("Size of a long long int: %lu byte(s)\n",(unsigned long)sizeof(long_double_type));
+printf("Size of a float: %lu byte(s)\n",(unsigned long)sizeof(float_type));
 return (0);
 }
