3444981ab8743b57373d23c9a7f3e403d7e73e84
Fortunatus <wulnyefortunatus@gmail.com>
2022-10-14 16:18:10 -0700

    Variables, decisons and loops

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,20 +1,22 @@
 #include <stdio.h>
-
+#include <time.h>
 
 /**
-
  * main -Entry point
-
  *
-
  * Return: Always 0 (Success)
-
  */
-
-int main(void){
-
-
-    return(0);
-
-
+int main(void)
+{
+int n;
+srand(time(0));
+n = rand() - RAND_MAX /2;
+int last_dig = n % 10;
+if (last_dig > 5)
+printf("Last digit of %d is %d and is greater than 5\n", n, last_dig);
+else if (last_dig == 5)
+printf("Last digit of %d is %d and is greater than 0\n", n, last_dig);
+else
+printf("Last digit of %d is %d and is less than 6 and not 0\n", n, n % 10);
+return(0);
 }
