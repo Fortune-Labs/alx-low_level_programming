8cf334406c1600d1b17639b42a9411cd4827a65e
Fortunatus <wulnyefortunatus@gmail.com>
2022-10-26 21:15:36 +0000

    All files added

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -1,27 +1,46 @@
 #include "main.h"
 
 /**
- * rot13 -  a function that encodes a string using rot13.
- * @s: An input string to encode using rot13
- * Return: An encode string
+ * rot13 - Encodes a string using rot13.
+ * @str: The string to be encoded.
+ *
+ * Return: A pointer to the encoded string.
  */
-char *rot13(char *s)
+char *rot13(char *str)
 {
-int a = 0;
+int indx1 = 0, indx2;
+char alphabet[52] = {'A', 'B', 'C', 'D', 'E', 'F',
+'G', 'H', 'I', 'J', 'K', 'L',
+'M', 'N', 'O', 'P', 'Q', 'R',
+ 'S', 'T', 'U', 'V', 'W', 'X',
+'Y', 'Z', 'a', 'b', 'c', 'd',
+'e', 'f', 'g', 'h', 'i', 'j',
+'k', 'l', 'm', 'n', 'o', 'p',
+'q', 'r', 's', 't', 'u', 'v',
+'w', 'x', 'y', 'z'};
+char rot13key[52] = {'N', 'O', 'P', 'Q', 'R', 'S',
+'T', 'U', 'V', 'W', 'X', 'Y',
+'Z', 'A', 'B', 'C', 'D', 'E',
+'F', 'G', 'H', 'I', 'J', 'K',
+'L', 'M', 'n', 'o', 'p', 'q',
+'r', 's', 't', 'u', 'v', 'w',
+'x', 'y', 'z', 'a', 'b', 'c',
+'d', 'e', 'f', 'g', 'h', 'i',
+'j', 'k', 'l', 'm'};
 
-while (s[a] != '\0')
+while (str[indx1])
 {
-while ((s[a] >= 'a' && s[a] <= 'z') ||
-(s[a] >= 'A' && s[a] <= 'Z'))
+for (indx2 = 0; indx2 < 52; indx2++)
 {
-if ((s[a] >= 'a' && s[a] <= 'm') ||
-(s[a] >= 'A' && s[a] <= 'M'))
-s[a] += 13;
-else
-s[a] -= 13;
-a++;
+if (str[indx1] == alphabet[indx2])
+{
+str[indx1] = rot13key[indx2];
+break;
 }
-a++;
 }
-return (s);
+
+indx1++;
+}
+
+return (str);
 }
