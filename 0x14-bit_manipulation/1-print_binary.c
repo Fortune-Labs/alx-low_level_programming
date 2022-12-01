aab83f1532c04df26bb3fa98d767d18b94f3812b
Fortunatus <wulnyefortunatus@gmail.com>
2022-12-01 22:31:03 +0000

    1-print_binary file added

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -5,10 +5,6 @@
  * @n: The number to be printed
  * Return: Nothing
  */
-/**
- * print_binary - Prints the binary representation of a number.
- * @n: The number to be printed in binary.
- */
 void print_binary(unsigned long int n)
 {
 	if (n > 1)
