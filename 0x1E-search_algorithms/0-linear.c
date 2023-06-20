264c05cf420d6f9d3d44a3263819c23b641e1cee
Fortunatus <wulnyefortunatus@gmail.com>
2023-06-20 22:53:04 +0000

    binary search

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -9,18 +9,18 @@
  *
  * Return: the index of first value match, -1 if not present or array is NULL
  */
+
 int linear_search(int *array, size_t size, int value)
 {
-	size_t i;
+	size_t i = 0;
 
-	if (array == NULL || size == 0)
+	if (array == NULL)
 		return (-1);
 
-	for (i = 0; i < size; i++)
+	for (; i < size; i++)
 	{
 		printf("Value checked array[%lu] = [%d]\n", i, array[i]);
 		if (array[i] == value)
 			return (i);
-	}
-	return (-1);
+	} return (-1);
 }
